034637f5a62a999dc6236cf87a43f169362d7e11
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2023-12-02 11:09:12 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Camera } from 'lucide-react';
+
+function App() {
+  return (
+    <>
+    <Camera size={64} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <ChakraProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </ChakraProvider>
+);
